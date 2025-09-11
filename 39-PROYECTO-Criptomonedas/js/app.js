e8a43b779e25c4e0e1d83f5ce91941ede0fa9ad0
ByCharliee <carloshc.1597@gmail.com>
2025-09-11 11:52:42 -0600

    Añadiendo nuevo proyecto. Cotizador de bitcoin en tiempo real

--- 39-PROYECTO-Criptomonedas/js/app.js
+++ 39-PROYECTO-Criptomonedas/js/app.js
@@ -1,7 +1,23 @@
-const selectCriptoMoneda = document.querySelector('#criptomoneda');
+const selectCripto = document.querySelector('#criptomoneda');
+const selectMoneda = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+const resultado = document.querySelector('#resultado');
+
+const objCambio = {
+    moneda: '',
+    criptomoneda: '',
+    nombreMoneda: '',
+    nombreCripto: ''
+}
+
 
 document.addEventListener('DOMContentLoaded',() => {
     buscarCriptomonedas();
+
+    formulario.addEventListener('submit', validarFormulario);
+    selectCripto.addEventListener('change', leerValor);
+    selectMoneda.addEventListener('change', leerValor);
+
  })
 
 
@@ -14,6 +30,7 @@ function buscarCriptomonedas(){
 }
 
 function agregarSelect(arrCripto){
+
     
     arrCripto.forEach(cripto => {
         const {Name, FullName} = cripto.CoinInfo;
@@ -21,7 +38,110 @@ function agregarSelect(arrCripto){
         const option = document.createElement('OPTION');
         option.value = Name;
         option.textContent = FullName;
-        selectCriptoMoneda.appendChild(option);
+        selectCripto.appendChild(option);
     });
     
+}
+
+function leerValor(e){
+    objCambio[e.target.name] = e.target.value;
+
+    //Extrae el nombre de la moneda en el Option seleccionado
+    const moneda = [...e.target.children].find(item => item.value === e.target.value);
+    objCambio[e.target.dataset.coin] = moneda.textContent;
+    
+    console.log(moneda.textContent);
+    console.log(e.target.dataset.coin);
+
+    console.log(objCambio);
+}
+
+function validarFormulario(e){
+    e.preventDefault();
+
+    const {moneda, criptomoneda} = objCambio;
+
+    //Verificar si hay alerta activa
+    if(document.querySelector('.alerta')){
+        return;
+    }
+
+    if(moneda === '' || criptomoneda === ''){
+        mostrarAlerta('Debes seleccionar ambos campos');
+        return;
+    }
+
+    consultarCotizacion(moneda, criptomoneda);
+
+    formulario.reset();
+
+
+}
+
+function mostrarAlerta(mensaje){
+    const divAlerta = document.createElement('DIV');
+    divAlerta.classList.add('alerta', 'error');
+    divAlerta.textContent = mensaje;
+
+    formulario.appendChild(divAlerta);
+
+    setTimeout(() => {
+        divAlerta.remove();
+    }, 1500);
+}
+
+function consultarCotizacion(moneda, criptomoneda){
+    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => mostrarCotizacion(data.DISPLAY[criptomoneda][moneda]));
+}
+
+function mostrarCotizacion(cotizacion){
+    limpiarHtml();
+
+    console.log(cotizacion);
+
+    const {CHANGEPCT24HOUR, HIGHDAY, LASTUPDATE, LOWDAY, PRICE} = cotizacion;
+
+    const descripcion = document.createElement('H2');
+    descripcion.classList.add('descripcion');
+    descripcion.textContent = `${objCambio.nombreMoneda}  -  ${objCambio.nombreCripto}`;
+
+    const precio = document.createElement('P');
+    precio.classList.add('precio');
+    precio.innerHTML = `El precio actual es de:<span>${PRICE}</span>`;
+
+    const precioAlto = document.createElement('P');
+    precioAlto.innerHTML = `El precio más alto es de:<span>${HIGHDAY}</span>`;
+
+    const precioBajo = document.createElement('P');
+    precioBajo.innerHTML = `El precio más bajo es de:<span>${LOWDAY}</span>`;
+
+    const variacion = document.createElement('P');
+    variacion.innerHTML = `Variación las últimas 24 horas:<span>${CHANGEPCT24HOUR}%</span>`;
+
+    const actualizacion = document.createElement('P');
+    actualizacion.innerHTML = `Última actualización:<span>${LASTUPDATE}</span>`;
+
+    
+
+
+
+    resultado.appendChild(descripcion);
+    resultado.appendChild(precio);
+    resultado.appendChild(precioAlto);
+    resultado.appendChild(precioBajo);
+    resultado.appendChild(variacion);
+    resultado.appendChild(actualizacion);
+
+
+
+}
+
+function limpiarHtml(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
 }
